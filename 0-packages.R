b98dc6e8a229a46884fa261a1d09661eecd5ee60
Kaizad <kaizad.patel@pnnl.gov>
2019-11-04 08:41:13 -0800

    misc changes

--- 0-packages.R
+++ 0-packages.R
@@ -60,7 +60,7 @@ theme_kp <- function() {  # this for all the elements common across plots
 gg_vankrev <- function(data,mapping){
   ggplot(data,mapping)+
 # plot points
-    geom_point(size=2, alpha = 0.4)+ # set size and transparency
+    geom_point(size=2, alpha = 0.5)+ # set size and transparency
 # axis labels
     ylab("H/C")+
     xlab("O/C")+
---
+++ 4-misc_script.R
@@ -0,0 +1,174 @@
+master %>% 
+  group_by(Treatment,Mass) %>% 
+  dplyr::summarise(intensity = mean(intensity)) %>% 
+  
+  
+  left_join(dplyr::select(meta_elements, O, Mass), by = "Mass") %>% 
+  dplyr::filter(Treatment=="PreFenton"| Treatment=="PostFenton")->master_el
+
+
+      #   Class             Treatment  median
+      #   <fct>             <fct>       <dbl>
+      # 1 Carbohydrate-like PostFenton     14
+      # 2 Carbohydrate-like PreFenton      13
+      # 3 Lignin-like       PostFenton     11
+      # 4 Lignin-like       PreFenton      10
+
+
+master_el %>% 
+  left_join(class, by = "Mass") %>%
+  filter(Class=="Carbohydrate-like"|Class=="Lignin-like") %>% 
+  group_by(Treatment,Class,O) %>% 
+  dplyr::summarise(count = n()) %>% 
+ 
+  ungroup %>% 
+  group_by(Treatment,Class) ->temp_master
+
+ggplot(temp_master[temp_master$Class=="Carbohydrate-like"| temp_master$Class=="Lignin-like",], aes(x = O, y =count, fill = Treatment))+
+  geom_bar(stat = "identity")+
+  facet_grid(Treatment~ Class)+
+  theme_kp()
+
+
+
+
+
+ggplot(master_el, aes(x = O, fill = Treatment))+
+  geom_histogram(binwidth = 1)+
+  facet_wrap(Forest~Treatment)
+
+
+ggplot(temp_master, aes(x = O, y = count, fill = Treatment))+
+  geom_bar(stat = "identity")+
+  facet_wrap(Treatment~.)
+
+
+master_el %>% 
+  group_by(Treatment) %>% 
+  dplyr::mutate(median = median(O))->master_el
+
+master_el %>% 
+  group_by(Treatment) %>% 
+  dplyr::summarise(n = n())
+
+
+sorbed %>% 
+  left_join(select(meta_elements,Mass,O), by = "Mass")->sorbed_temp
+
+sorbed_temp %>% 
+  group_by(adsorbed) %>% 
+  dplyr::summarise(median = median(O))
+
+
+
+
+ggplot(sorbed_temp[sorbed_temp$adsorbed=="sorbed"&(temp$Class=="Carbohydrate-like"|temp$Class=="Lignin-like"),], aes(x = O, fill = adsorbed))+
+  geom_histogram(binwidth = 1)+
+  facet_grid(Forest+fenton~adsorbed+Class)
+
+
+fenton_el %>% 
+  left_join(class, by = "Mass") %>% 
+  group_by(Class, Forest, Treatment, loss) %>% 
+  dplyr::summarize(median = median(O))->temp
+
+
+ggplot(temp, aes(x = O, fill = loss))+
+  geom_histogram()+
+  facet_grid(Forest+loss~Class)
+
+
+
+
+#### molecular analysis of fenton loss-gained molecules of carb and lignin
+
+
+fenton_el %>%
+  group_by(Mass,loss) %>% 
+  dplyr::summarise(HC = mean(HC),
+                   OC = mean(OC),
+                   O = mean(O)) %>% 
+  left_join(class, by = "Mass") %>% 
+  filter(Class=="Carbohydrate-like"|Class=="Lignin-like") ->fenton_temp1
+
+fenton_temp1 %>% 
+  group_by(loss) %>% 
+  dplyr::summarize(median = median(O),
+                   n = n())
+
+
+    #   Class             loss      median
+    #   <fct>             <fct>      <dbl>
+    # 1 Carbohydrate-like conserved     14
+    # 2 Carbohydrate-like gained        14
+    # 3 Carbohydrate-like lost          11
+    # 4 Lignin-like       conserved     10
+    # 5 Lignin-like       gained        14
+    # 6 Lignin-like       lost           9
+
+
+fenton_temp1%>% 
+  group_by(Class,loss, O) %>% 
+  dplyr::summarise(count = n()) %>% 
+  dplyr::mutate(count2 = if_else(loss=="lost",-1*count,as.numeric(count)))->fenton_temp2
+
+
+ggplot(fenton_temp[!fenton_temp$loss=="conserved",], aes(x = O, fill = loss))+
+  geom_histogram(binwidth = 1)+
+  facet_grid(Class~loss)
+  
+
+
+ggplot(fenton_temp2[!fenton_temp2$loss=="conserved",], aes(x=as.factor(O), y=count2, fill=loss))+
+  geom_bar(stat = "identity", alpha=0.8)+
+  scale_fill_brewer(palette="Dark2")+
+  scale_x_discrete(breaks = c("0", "5","10","15","20","25","30"))+
+  ylab("number of peaks")+
+  xlab("number of O")+
+  facet_wrap(~Class)+
+  theme_kp()
+
+
+#### molecular analysis of sorbed
+sorbed %>%
+  group_by(Mass,fenton,adsorbed) %>% 
+  dplyr::summarise(HC = mean(HC),
+                   OC = mean(OC))%>% 
+  left_join(class, by = "Mass") %>%
+  left_join(select(meta_elements,Mass,O),  by = "Mass") %>% 
+  filter(Class=="Carbohydrate-like"|Class=="Lignin-like") ->sorbed_temp1
+
+
+sorbed_temp1%>% 
+  group_by(Class,adsorbed, fenton, O) %>% 
+  dplyr::summarise(count = n()) ->sorbed_temp2
+
+
+ggplot(sorbed_temp2[sorbed_temp2$adsorbed=="sorbed",], aes(x=as.factor(OC), y=count, fill=fenton))+
+  geom_bar(stat = "identity", alpha=0.8)+
+  scale_fill_brewer(palette="Dark2")+
+#  scale_x_discrete(breaks = c("0", "5","10","15","20","25","30"))+
+ # scale_x_continuous(limits = c(0,30))+
+  ylab("number of peaks")+
+  xlab("number of O")+
+  facet_grid(fenton~Class)+
+  theme_kp()
+
+
+sorbed_temp1 %>% 
+  group_by(adsorbed, fenton,Class) %>% 
+  dplyr::summarize(median = median(O)) 
+
+
+### sorbed -- comparing only peaks seen in the original extracts
+
+sorbed %>% 
+  spread(fenton, adsorbed)%>% 
+  filter(!is.na(PreFenton)) %>% 
+  gather(key = "fenton", value ="sorbed", PreFenton:PostFenton) %>% 
+  filter(Class=="Carbohydrate-like"|Class=="Lignin-like") ->sorbed_initial_temp
+
+
+gg_vankrev(na.omit(sorbed_initial_temp), aes(x = OC, y=HC, color = sorbed))+
+  facet_grid(fenton~Class)
+
